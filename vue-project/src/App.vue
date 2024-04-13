22725c4f811ffd1ee09d78c187eb7c512606d37d
Frederik Woloszynski <frederikwoloszynski@hotmail.com>
2024-04-13 07:16:06 +0200

    New lists, and tasks with editing feature

---
+++ vue-project/src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <div class="header">
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+      </nav>
+    </div>
+  </header>
+  <div class="wrapper">
+    <RouterView />
+  </div>
+</template>
+
+<style scoped>
+  @import './assets/base.css';
+</style>
