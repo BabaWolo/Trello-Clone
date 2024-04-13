6b35bd41561906208757eb1172ac56608ceefff9
Frederik Woloszynski <frederikwoloszynski@hotmail.com>
2024-04-13 11:14:50 +0200

    Drag feature

--- vue-project/src/components/KanbanBoard.vue
+++ vue-project/src/components/KanbanBoard.vue
@@ -1,14 +1,14 @@
 <template>
   <v-container fluid>
     <v-row>
-      <v-col cols="4" v-for="(list, index) in lists" :key="index">
+      <v-col cols="3" v-for="(list, index) in lists" :key="index">
         <div v-if="!list.isEditing">
           <h2 @dblclick="enableEditing(index)">{{ list.title }}:</h2>
           <v-menu>
             <template v-slot:activator="{ props }">
-              <v-btn icon="mdi-dots-horizontal" v-bind="props"></v-btn>
+              <v-btn class="sub-menu-btn" icon="mdi-dots-horizontal" v-bind="props"></v-btn>
             </template>
-            <v-list>
+            <v-list class="sub-menu">
               <v-list-item @click="deleteList(index)">
                 <v-list-item-title>Delete</v-list-item-title>
               </v-list-item>
@@ -29,30 +29,37 @@
           <v-text-field v-model="list.title" @keyup.enter="disableEditing(index)" autofocus></v-text-field>
           <v-btn @click="disableEditing(index)">Save</v-btn>
         </div>
+        <draggable v-model="list.items" :group="{ name: 'board' }">
         <v-card
-          v-for="(item, itemIndex) in list.description"
+          v-for="(item, itemIndex) in list.items"
           :key="itemIndex"
           class="mb-4"
         >
-          <v-card-text>{{ item.description }}</v-card-text>
-          <v-card-actions>
-            <v-menu>
-              <template v-slot:activator="{ props }">
-                <v-btn icon="mdi-dots-horizontal" v-bind="props"></v-btn>
-              </template>
-              <v-list>
-                <v-list-item @click="editItem(index, itemIndex)">
-                  <v-list-item-title>Edit</v-list-item-title>
-                </v-list-item>
-                <v-list-item @click="deleteItem(index, itemIndex)">
-                  <v-list-item-title>Delete</v-list-item-title>
-                </v-list-item>
-              </v-list>
-            </v-menu>
-          </v-card-actions>
+          <v-card-text v-if="!item.isEditing" @dblclick="editItem(index, itemIndex)">{{ item.items }}
+            <v-card-actions>
+              <v-menu>
+                <template v-slot:activator="{ props }">
+                  <v-btn icon="mdi-dots-horizontal" v-bind="props"></v-btn>
+                </template>
+                <v-list>
+                  <v-list-item @click="editItem(index, itemIndex)">
+                    <v-list-item-title>Edit</v-list-item-title>
+                  </v-list-item>
+                  <v-list-item @click="deleteItem(index, itemIndex)">
+                    <v-list-item-title>Delete</v-list-item-title>
+                  </v-list-item>
+                </v-list>
+              </v-menu>
+            </v-card-actions>
+          </v-card-text>
+          <v-text-field v-else v-model="item.items" @keyup.enter="disableItemEditing(index, itemIndex)" @keyup.esc="cancelItemEditing(index, itemIndex)" autofocus>
+          <v-btn @click="disableItemEditing(index, itemIndex)">Save</v-btn>
+          <v-btn @click="cancelItemEditing(index, itemIndex)"><v-icon>mdi-close</v-icon></v-btn>
+          </v-text-field>
         </v-card>
+        </draggable>
         <div v-if="list.isAddingTask">
-          <v-text-field v-model="list.newTaskTitle" @keyup.enter="addTask(index)" autofocus></v-text-field>
+          <v-text-field v-model="list.newTaskTitle" @keyup.enter="addTask(index)" @keyup.esc="cancelAddTask(index)" autofocus></v-text-field>
           <v-btn @click="addTask(index)">Add Card</v-btn>
           <v-btn @click="cancelAddTask(index)"><v-icon>mdi-close</v-icon></v-btn>
         </div>
@@ -66,18 +73,24 @@
 </template>
 
 <script lang="ts">
-
-export default {
+import { defineComponent } from 'vue'
+import { VueDraggableNext } from 'vue-draggable-next'
+export default defineComponent({
+  components: {
+    draggable: VueDraggableNext,
+  },
   data() {
     return {
-      lists: [{ title: 'New Task', description: [], isEditing: false, newTaskTitle: '', isAddingTask: false},
+      lists: [{ title: 'Todo', items: [], isEditing: false, newTaskTitle: '', isAddingTask: false},
+      { title: 'In Progress', items: [], isEditing: false, newTaskTitle: '', isAddingTask: false},
+      { title: 'Done', items: [], isEditing: false, newTaskTitle: '', isAddingTask: false},
       // more tasks...
       ],
     }
   },
   methods: {
     addTask(listIndex) {
-      this.lists[listIndex].description.push({ description: this.lists[listIndex].newTaskTitle });
+      this.lists[listIndex].items.push({ items: this.lists[listIndex].newTaskTitle });
       this.lists[listIndex].isAddingTask = false;
       this.lists[listIndex].newTaskTitle = '';
     },
@@ -89,16 +102,17 @@ export default {
       this.lists[listIndex].isAddingTask = true;
     },
     editItem(listIndex, itemIndex) {
-      // Edit the item at itemIndex in the list at listIndex...
+      this.lists[listIndex].items[itemIndex].originalItems = this.lists[listIndex].items[itemIndex].items;
+      this.lists[listIndex].items[itemIndex].isEditing = true;
     },
     deleteItem(listIndex, itemIndex) {
-      this.lists[listIndex].description.splice(itemIndex, 1);
+      this.lists[listIndex].items.splice(itemIndex, 1);
     },
     deleteList(index) {
       this.lists.splice(index, 1);
     },
     addList() {
-      this.lists.push({ title: 'New Task', description: [] });
+      this.lists.push({ title: 'New Task', items: [] });
     },
     enableEditing(index) {
       this.lists[index].isEditing = true;
@@ -106,6 +120,13 @@ export default {
     disableEditing(index) {
       this.lists[index].isEditing = false;
     },
+    disableItemEditing(listIndex, itemIndex) {
+      this.lists[listIndex].items[itemIndex].isEditing = false;
+    },
+    cancelItemEditing(listIndex, itemIndex) {
+      this.lists[listIndex].items[itemIndex].items = this.lists[listIndex].items[itemIndex].originalItems;
+      this.lists[listIndex].items[itemIndex].isEditing = false;
+    },
   },
-}
+})
 </script>
